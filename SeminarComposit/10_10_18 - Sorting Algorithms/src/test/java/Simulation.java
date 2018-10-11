b6bbd7def07d8618acf90f4c4f123fc6bf14cefc
Janik Mayr <jan.222.ik@gmail.com>
2018-10-11 21:51:54 +0200

    Seminar Testcase CleanUp

--- SeminarComposit/10_10_18 - Sorting Algorithms/src/test/java/Simulation.java
+++ SeminarComposit/10_10_18 - Sorting Algorithms/src/test/java/Simulation.java
@@ -1,7 +1,5 @@
 import org.junit.Test;
 
-import java.time.LocalTime;
-import java.time.Period;
 import java.util.Random;
 
 /**
@@ -10,34 +8,36 @@ import java.util.Random;
 public class Simulation {
     @Test
     public void bubbleSort() {
+        TESTDATA[] input = new TESTDATA[1000];
+        Random random = new Random();
+        for (int i = 0; i < input.length; i++) {
+            input[i] = new TESTDATA(random.nextInt()%input.length);
+        }
+        Sorter<TESTDATA> sorter = new Sorter<>();
+        TESTDATA[] sort = sorter.sort(input.clone(), new TESTDATAComp(), new BubbleSort());
+        printConsole(input,sort,true);
+    }
+    @Test
+    public void quickSort() {
         TESTDATA[] input = new TESTDATA[100000];
         Random random = new Random();
         for (int i = 0; i < input.length; i++) {
             input[i] = new TESTDATA(random.nextInt()%input.length);
         }
-        Sorter<TESTDATA> sorter = new Sorter<TESTDATA>();
-        TESTDATA[] sort = sorter.sort(input.clone(), new TESTDATAComp(), new BubbleSort());
-        for (int i = 0; i < input.length; i++) {
-            System.out.println("INDEX[" + i + "] OLD: " + input[i].number + " NEW: " + sort[i].number);
-        }
-    }
-    @Test
-    public void quickSort() {
-        TESTDATA[] input = new TESTDATA[10000000];
-        Random random = new Random();
-        for (int i = 0; i < input.length; i++) {
-            input[i] = new TESTDATA(random.nextInt()%input.length);
-        }
         long time = System.currentTimeMillis();
-        Sorter<TESTDATA> sorter = new Sorter<TESTDATA>();
+        Sorter<TESTDATA> sorter = new Sorter<>();
         TESTDATA[] sort = sorter.sort(input.clone(), new TESTDATAComp(), new QuickSort());
         long time2 = System.currentTimeMillis();
         long t3 = time2 -time;
         System.out.println("Milli: " + t3);
-        /*
-        for (int i = 0; i < input.length; i++) {
-            System.out.println("OLD: " + input[i].number + " NEW: " + sort[i].number);
+        printConsole(input,sort,false);
+    }
+
+    private void printConsole(TESTDATA[] unsorted, TESTDATA[] sorted, boolean sort) {
+        if (sort) {
+            for (int i = 0; i < unsorted.length; i++) {
+                System.out.println("INDEX[" + i + "] OLD: " + unsorted[i].number + " NEW: " + sorted[i].number);
+            }
         }
-        */
     }
 }
